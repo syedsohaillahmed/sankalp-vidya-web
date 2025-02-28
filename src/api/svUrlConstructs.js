ee8bb258db9627cb3fe2f00d975fa6a02842f422
syedsohaillahmed <syedsohaillahmed19@gmail.com>
2025-02-28 16:17:42 +0530

    updated url with new updated url changes

--- src/api/svUrlConstructs.js
+++ src/api/svUrlConstructs.js
@@ -1,9 +1,15 @@
 const baseUrl = "http://localhost:8000/api/v1"
 
 export const getUserDetailsByIdUC= (userId)=>{
-    return `${baseUrl}/users/${userId}`
+    return `${baseUrl}/users/user/${userId}`
 }
 
+// http://localhost:8000/api/v1/users/user/67b20d55a6d874eb70ee2609
+// http://localhost:8000/api/v1/users/user/67b20d55a6d874eb70ee2609
+
+
+
+
 export const getUserListdUC= ()=>{
-    return `${baseUrl}/users`
+    return `${baseUrl}/users/userList`
 }
